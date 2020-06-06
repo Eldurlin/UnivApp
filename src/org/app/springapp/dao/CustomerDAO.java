129c3d12803b0c1fa7b5a6bb40e828591453976a
AC <adam.cakala@gmail.com>
2020-06-06 19:19:19 +0200

    first commit

---
+++ src/org/app/springapp/dao/CustomerDAO.java
@@ -0,0 +1,10 @@
+package org.app.springapp.dao;
+
+import java.util.List;
+
+import org.app.springapp.entity.Customer;
+
+public interface CustomerDAO {
+
+	public List<Customer> getCustomers();
+}
---
+++ src/org/app/springapp/dao/CustomerDAOImpl.java
@@ -0,0 +1,17 @@
+package org.app.springapp.dao;
+
+import java.util.List;
+
+import org.app.springapp.entity.Customer;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	@Override
+	public List<Customer> getCustomers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
