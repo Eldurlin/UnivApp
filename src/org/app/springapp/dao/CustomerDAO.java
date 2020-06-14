b1b3b73b807c65dd2b35486448ed24829774b2bd
Piotr <szewczyk.piotr1503@gmail.com>
2020-06-14 19:29:16 +0200

    CustomerDAO

--- src/org/app/springapp/dao/CustomerDAO.java
+++ src/org/app/springapp/dao/CustomerDAO.java
@@ -7,4 +7,11 @@ import org.app.springapp.entity.Customer;
 public interface CustomerDAO {
 
 	public List<Customer> getCustomers();
+
+	public void saveCustomer(Customer theCustomer);
+
+	public Customer getCustomer(int theId);
+
+	public void deleteCustomer(int theId);
+	
 }
